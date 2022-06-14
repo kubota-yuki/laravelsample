c872e1df15a613b5a09a8dd76b56a923ea4237eb
Koichiro2080 <koichiro2080@gmail.com>
2022-06-14 15:42:55 +0900

    かくにん１

--- myapp/resources/views/Hello/add.blade.php
+++ myapp/resources/views/Hello/add.blade.php
@@ -2,10 +2,10 @@
  <from action="hello/add" method="post">
     <table>
         @csrf
-        <tr><th>id</th><td><input type= "text" name = "id"></td></tr>
-        <tr><th>name</th><td><input type= "text" name = "name"></td></tr>
-        <tr><th>birthday</th><td><input type= "text" name = "birthday"></td></tr>
-        <tr><th>birthplace</th><td><input type= "text" name = "birthplace"></td></tr>
+        <tr><th>id:</th><td><input type= "text" name = "id"></td></tr>
+        <tr><th>name:</th><td><input type= "text" name = "name"></td></tr>
+        <tr><th>birthday:</th><td><input type= "text" name = "birthday"></td></tr>
+        <tr><th>birthplace:</th><td><input type= "text" name = "birthplace"></td></tr>
         <tr><th></th><td><input type="submit" value="send"></td></tr>
     </table>
  </from>

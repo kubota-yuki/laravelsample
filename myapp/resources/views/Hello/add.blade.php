190b039f3350c3345c1e304017dc839f7799b2d4
Koichiro2080 <koichiro2080@gmail.com>
2022-06-15 09:48:01 +0900

    hiddenに変えてみた

--- myapp/resources/views/Hello/add.blade.php
+++ myapp/resources/views/Hello/add.blade.php
@@ -2,7 +2,7 @@
  <from action="hello/add" method="post">
     <table>
         @csrf
-        <tr><th>id:</th><td><input type= "text" name = "id"></td></tr>
+        <tr><th>id:</th><td><input type= "hidden" name = "id"></td></tr>
         <tr><th>name:</th><td><input type= "text" name = "name"></td></tr>
         <tr><th>birthday:</th><td><input type= "text" name = "birthday"></td></tr>
         <tr><th>birthplace:</th><td><input type= "text" name = "birthplace"></td></tr>

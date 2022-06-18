e9121189f3ac7ebb6d0d9123dd29b62557a4521b
Koichiro Tsuru <100543788+Koichiro2080@users.noreply.github.com>
2022-06-19 00:29:08 +0900

    checkのとこ

--- myapp/app/Http/Controllers/MusicController.php
+++ myapp/app/Http/Controllers/MusicController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class MusicController extends Controller
 {
-    //
+    public function show(Request $request){
+        $id = $request->id;
+        $item = DB::table('データベース')->where('キー', $キー)->first();
+            return view('hello.check',['item' => $item]);
+        }
 }
--- myapp/resources/views/music/check.blade.php
+++ myapp/resources/views/music/check.blade.php
@@ -1,2 +1,9 @@
 <h1>確認画面</h1>
 
+<table>
+    <tr><th>その１</th><th>{{$item->}}</th></tr>
+    <tr><th>その２</th><th>{{$item->}}</th></tr>
+    <tr><th>その３</th><th>{{$item->}}</th></tr>
+    <tr><th>その４</th><th>{{$item->}}</th></tr>
+    <tr><th><a href="/music">進む</a></th></tr>
+</table>

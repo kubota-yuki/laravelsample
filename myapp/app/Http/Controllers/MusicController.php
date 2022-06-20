f84e5a9fedd5de86d11e8473e341b50d5d5b828f
Koichiro2080 <koichiro2080@gmail.com>
2022-06-20 09:40:26 +0900

    a

--- myapp/app/Http/Controllers/MusicController.php
+++ myapp/app/Http/Controllers/MusicController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class MusicController extends Controller
 {

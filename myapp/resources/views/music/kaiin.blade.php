5a88083d03248de7786ffec661add930aed357b7
yamazaki-shota <xshanqi5@gmail.com>
2022-06-20 10:46:25 +0900

    会員登録

---
+++ myapp/resources/views/music/kaiin.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.helloapp')
+
+@section('title','ユーザー認証')
+
+@section('menuber')
+   @parent
+   ユーザー認証ページ
+@endsection
+
+@section('content')
+<p>{{$message}}</p>
+    <from action="/music/kaiin" method="post">
+    <table>
+        @csrf
+        <tr><th>mail: </th><td><input type="text"
+               name="email"></td></tr>
+        <tr><th>pass: </th><td><input type="password"
+               name="password"></td></tr>
+        <tr><th></th><td><input type="submit"
+               value="send"></td></tr>
+    </table>
+    </form>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection

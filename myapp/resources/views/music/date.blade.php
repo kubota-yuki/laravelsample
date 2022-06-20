05176c28a6894e78848837d696e8b391becbf92c
Koichiro2080 <koichiro2080@gmail.com>
2022-06-20 11:49:44 +0900

    nannkairoiro

--- myapp/resources/views/music/date.blade.php
+++ myapp/resources/views/music/date.blade.php
@@ -12,8 +12,10 @@
 @else
 <p>未ログイン</p> <a href ="/music/loguin">ログイン</a>|<a href="/music/register">会員登録</a>
 @endif
+<form action="/music/date" method="post">
 <h1>予約したい日にちを入力してください</h1>
-<input type="date"><br>
+<input type="date" name = date><br>
 <a href="/music/choose">次へ</a>
+</form>
 </body>
 </html>
